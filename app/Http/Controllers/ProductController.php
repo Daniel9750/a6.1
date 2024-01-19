d3fcaae9a7ee6bd691b27ceab6b49e31e4cf857c
Daniel Sánchez González <danielsanchezgonzalez@alumno.ieselrincon.es>
2024-01-19 11:47:26 +0000

    product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+
+<?php
+/*
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function list() {
+        $viewData = [];
+        $viewData["title"] = "Página principal - Tienda online";
+
+        return view("product.list")->with("viewData", $viewData);
+    }
+
+    // Controlador de la página "Acerca de"
+    public function about() {
+        $viewData = [];
+        $viewData["title"] = "Acerca de - Tienda Online";
+        $viewData["subtitle"] =  "Acerca de";
+        $viewData["description"] =  "Esta es la página acerca de ...";
+        $viewData["author"] = "Desarrollado por: DSW";
+
+        return view("home.about")->with("viewData", $viewData);
+    }
+}
+*/
