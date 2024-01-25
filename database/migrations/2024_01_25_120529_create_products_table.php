0afee1a83e91d7bb8a6b05d09a6b2b622346d708
Daniel Sánchez González <danielsanchezgonzalez@alumno.ieselrincon.es>
2024-01-25 12:34:30 +0000

    a6.2

--- database/migrations/2024_01_25_120529_create_products_table.php
+++ database/migrations/2024_01_25_120529_create_products_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
 
+            $table->string("name");
+            $table->text("description");
+            $table->string("image");
+            $table->double("price", 8, 2);
 
 
-
-            
             $table->timestamps();
         });
     }
